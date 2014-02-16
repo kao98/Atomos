56e1f0e6a4dcd2d6c12debba3375475003ea2824
aurelien <aurelien@knt-st-lx>
2014-02-16 22:12:06 +0100

    Cleaning up Graphics Engine + adding Controls Helper

--- public_html/js/app/engine/Graphics.js
+++ public_html/js/app/engine/Graphics.js
@@ -1,218 +1,322 @@
 
-/*jslint                                    */
-/*global define, window, document, THREE    */
+/*jslint                                                             */
+/*global define, window, document, THREE, LOGGER, _LI, _LD, _LE, _LC */
 
 define([
-//    "lib/Logger",
-//    "GraphicObject",
-    "lib/three"  //the 3d library
+    "conf/config",
+    "lib/Logger",
+    "lib/three"
 
-], function (/*Logger, GraphicObject*/) {
+], function (config) {
     "use strict";
 
+    /**
+     * Graphics engine constructor
+     * @returns {_L10.GraphicsEngine}
+     */
     var GraphicsEngine = function () {
 
-        this.width = window.innerWidth;
+        this.devMode = config.devMode || false;
+
+        _LI("==> Initializing the graphics engine");
+        if (this.devMode) {
+            _LD("    == devMode activated == ");
+        }
+
+        this.width  = window.innerWidth;
         this.height = window.innerHeight;
 
-        this.gameScene = null;
-        this.scene = null;
-        this.camera = null;
-        this.renderer = null;
-
-        this
-            .initRenderer()
+        this.gameScene          = null;
+        this.scene              = null;
+        this.camera             = null;
+        this.renderer           = null;
+        this.controlsHelper     = null;
+        
+        this.initRenderer()
             .initScene()
-            .initCamera();
+            .initCamera()
+            .initControlsHelper();
 
         document.body.appendChild(this.renderer.domElement);
 
+        _LI("<== Graphics engine shoud be initialized now.");
+        
     };
 
+    /**
+     * Initialize the THREE scene
+     * @returns {_L10.GraphicsEngine.prototype}
+     */
     GraphicsEngine.prototype.initScene = function () {
 
+        _LD("    >Initializing the scene...");
+
         this.scene = new THREE.Scene();
-        this.scene.add(new THREE.AxisHelper(25));
-
+        
+        if (this.devMode) {
+            this.scene.add(new THREE.AxisHelper(100));
+        }
+//TODO: add lights in the scene and not in the graphics engine
         var ambientLight        = new THREE.AmbientLight(0x555555),
-            directionalLight    = new THREE.DirectionalLight(0xff0000);
+            directionalLight    = new THREE.DirectionalLight(0xffffff);
 
-        directionalLight.position.set(-.1, .25, -1).normalize();
+        directionalLight.position.set(-0.1, 0.25, -1).normalize();
 
         this.scene.add(ambientLight);
         this.scene.add(directionalLight);
 
-        this.scene.add(new THREE.DirectionalLightHelper(directionalLight, 2));
-
-        this.scene.add(plane);
+        _LD("    <Scene initialized");
 
         return this;
 
     };
 
+    /**
+     * Initialize the THREE main camera
+     * @returns {_L10.GraphicsEngine.prototype}
+     */
     GraphicsEngine.prototype.initCamera = function () {
 
-        this.camera = new THREE.PerspectiveCamera(25, this.width / this.height, 1, 1000);
+        _LD("    >Initializing the camera...");
 
-        this.camera.position.set(0, 25, 200);
-        this.camera.lookAt(this.scene.position);
+        this.camera = new THREE.PerspectiveCamera(65, this.width / this.height, 1, 10000);
+//TODO: the camera should be set from the scene or not?
+        this.camera.position.set(0, -400, 2500);
+        this.camera.lookAt({x: 0, y: -150, z: 0});
         this.scene.add(this.camera);
+
+        _LD("    <Camera initialized.");
 
         return this;
 
     };
 
+    /**
+     * Initialize the THREE renderer
+     * @returns {_L10.GraphicsEngine.prototype}
+     */
     GraphicsEngine.prototype.initRenderer = function () {
+
+        _LD("    >Initializing the renderer...");
 
         this.renderer = new THREE.CanvasRenderer();
         this.renderer.setSize(this.width, this.height);
+        
+//TODO: set the clear color in the scene, depending levels
+        this.renderer.setClearColor(0x020d30);
+
+        _LD("    <Renderer initialized.");
 
         return this;
     };
 
-    GraphicsEngine.prototype.initGlobalLights = function () {
+    /**
+     * Initialize the controlsHelper 
+     * that helps to handle object translation from the mouse
+     * @returns {undefined}
+     */
+    GraphicsEngine.prototype.initControlsHelper = function () {
+        
+        _LD("    >Initializing the controls helper...");
 
-        var ambientLight        = new THREE.AmbientLight(0x555555),
-            directionalLight    = new THREE.DirectionalLight(0xffffff);
+        var engine = this;
+        
+        //The controlsHelper is implemented in a static way
+        
+        this.controlsHelper = {
 
-        directionalLight.position.set(-0.5, 0.5, -1.5).normalize();
 
-        this.globalLights.push(ambientLight);
-        this.globalLights.push(directionalLight);
+            projector   : new THREE.Projector(),            
+            directions  : new THREE.Vector3(1, 1, 1),
+            object      : null,
+        
+            plane       : new THREE.Mesh(
+                    
+                new THREE.PlaneGeometry(3000, 3000, 30, 30),
+                new THREE.MeshBasicMaterial({
+                    color       : 0x000000,
+                    opacity     : 0.25,
+                    transparent : true,
+                    wireframe   : true
+                })
+                        
+            ),
+    
+            /**
+             * Attach an object to the controls helper, with the movable directions
+             * @param {type} object
+             * @param {type} directions
+             * @returns {undefined}
+             */
+            attach      : function (object, directions) {
+                
+                _LD("    >ControlsHelper: attaching an object to the controls helper...");
+                
+                if (object.desiredScreenPosition === undefined || object.setDesiredWorldPosition === undefined) {
+                    this.object = null;
+                    _LE("    /!\\ Improper object attached to the controls helper !");
+                } else {
 
-        return this;
+                    this.object = object;
+
+                    if (directions) {
+                        this.directions = new THREE.Vector3(
+                            directions.x !== 0 ? 1 : 0,
+                            directions.y !== 0 ? 1 : 0,
+                            directions.z !== 0 ? 1 : 0
+                        );
+                    }
+                    
+                    _LD("      Object '" + object.name + "' attached to the controls helper");
+                    _LD("      Directions: ", this.directions);
+                }
+                _LD("    <ControlsHelper: attaching an object to the controls helper done.");
+            },
+            
+            /**
+             * Check if we wan't to move the attached object, then calculate
+             * the position.
+             * @returns {undefined}
+             */
+            frame: function () {
+                
+                if (this.object && this.object.desiredScreenPosition !== null) {
+
+                    _LC("    >ControlsHelper: new position desired: ", this.object.desiredScreenPosition);
+
+                    var raycaster,
+                        intersects,
+                        vector = new THREE.Vector3(
+                            this.object.desiredScreenPosition.x * this.directions.x,
+                            this.object.desiredScreenPosition.y * this.directions.y,
+                            this.object.desiredScreenPosition.z * this.directions.z
+                        );
+                                                
+                    this.projector.unprojectVector(vector, engine.camera);
+                    
+                    raycaster = new THREE.Raycaster(
+                        engine.camera.position,
+                        vector.sub(engine.camera.position).normalize()
+                    );
+
+                    intersects = raycaster.intersectObject(this.plane);
+                    
+                    if (intersects && intersects.length > 0) {
+                    
+                        this.object.desiredScreenPosition = null;
+                        this.object.setDesiredWorldPosition(intersects[0].point);
+                        
+                        _LC("    <ControlsHelper: new disired position calculated: ", this.object.desiredWorldPosition);
+                        
+                    } else {
+                        
+                        _LC("    <ControlsHelper: no new disired position (out of bounds).");
+                        
+                    }
+
+
+
+                }
+            }
+        };
+        
+        
+        if (this.devMode) {
+            this.controlsHelper.plane.visible = true;
+        } else {
+            this.controlsHelper.plane.visible = false;
+        }
+
+        this.scene.add(this.controlsHelper.plane);
+        
+        _LD("    <Controls helper initialized.");
+        
     };
 
-    GraphicsEngine.prototype.initGlobalObjects = function () {
 
-        var plane,
-            areaTop,
-            areaLeft,
-            areaRight,
-            areaBottom;
-
-        plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(100, 50),
-            new THREE.MeshLambertMaterial({color: 0x020d30, overdraw: true})
-        );
-//        plane.position.z = 100;
-        plane.position.y = -15;
-        //plane.rotation.x = Math.PI;
-
-        this.globalObjects.push(plane);
-//
-this.globalObjects.push(new THREE.AxisHelper(25));
-
-
-//
-//        areaTop = new THREE.Mesh(
-//            new THREE.CubeGeometry(480, 5, 10),
-//            new THREE.MeshLambertMaterial({color: '#02300d', opacity: 1, transparent: false})
-//        );
-//        areaTop.position.z = 50;
-//        areaTop.position.y = 10;
-//
-//        areaLeft = new THREE.Mesh(
-//            new THREE.CubeGeometry(5, this.height - 20, 10),
-//            new THREE.MeshLambertMaterial({color: '#02300d', opacity: 1, transparent: false})
-//        );
-//        areaLeft.position.z = 50;
-//        areaLeft.position.x = -245;
-//        areaLeft.position.y = this.height / 2 + 5;
-//
-//        areaRight = new THREE.Mesh(
-//            new THREE.CubeGeometry(5, this.height - 20, 10),
-//            new THREE.MeshLambertMaterial({color: '#02300d', opacity: 1, transparent: false})
-//        );
-//        areaRight.position.z = 50;
-//        areaRight.position.x = 245;
-//        areaRight.position.y = this.height / 2 + 5;
-//
-//        areaBottom = new THREE.Mesh(
-//            new THREE.CubeGeometry(485, 5, 10),
-//            new THREE.MeshLambertMaterial({color: '#300202', opacity: 1, transparent: false, emissive: '#330000'})
-//        );
-//        areaBottom.position.z = 50;
-//        areaBottom.position.y = 640;
-//
-//        this.globalObjects.push(areaTop);
-//        this.globalObjects.push(areaLeft);
-//        this.globalObjects.push(areaRight);
-//        this.globalObjects.push(areaBottom);
-
-        return this;
-    };
-
+    /**
+     * Load the given scene
+     * @param {type} scene
+     * @returns {undefined}
+     */
     GraphicsEngine.prototype.loadScene = function (scene) {
+        
+        _LD("    >GraphicsEngine::loadScene: load the scene: ", scene);
+        
         this.gameScene = scene;
         this.loadComponents(this.gameScene.gameObjects);
+        
+        _LD("    <GraphicsEngine::loadScene: scene loaded.");
+        
     };
 
+    /**
+     * Load the given components
+     * @param {type} components
+     * @returns {undefined}
+     */
     GraphicsEngine.prototype.loadComponents = function (components) {
+
+        _LD("    >GraphicsEngine::loadComponents: ", components);
 
         var i = 0;
         for (i; i < components.length; i++) {
+            
             if (components[i].components && components[i].components.length > 0) {
+                //The component is not the object itself, but is composed of many components
                 this.loadComponents(components[i].components);
+                
             } else {
+                //The compoenent is a loadable object
                 this.loadObject(components[i]);
             }
+            
         }
+        
+        _LD("    <GraphicsEngine::loadComponents.");
+        
     };
 
+    /**
+     * Load scene objects
+     * @param {type} object
+     * @returns {undefined}
+     */
     GraphicsEngine.prototype.loadObject = function (object) {
+
+        _LD("    >GraphicsEngine::loadObject: ", object);
+
+//TODO: implement the proper to load objects
 
         var renderable = new THREE.Mesh(
             new THREE.CubeGeometry(object.properties.width, object.properties.height, object.properties.depth),
             new THREE.MeshLambertMaterial({color: object.properties.color, opacity: 1, transparent: false })
         );
+
         renderable.position.x = object.properties.x;
         renderable.position.y = object.properties.y;
-        renderable.position.z = 50;
+        renderable.position.z = 0;
 
-        //this.scene.add(renderable);
+        this.scene.add(renderable);
+        
         object.renderable = renderable;
 
-//        switch (object.properties.type) {
-//        case "plane":
-//            var plane = new THREE.Mesh(
-//                new THREE.PlaneGeometry(this.width, this.height, 1, 1),
-//                new THREE.MeshLambertMaterial({color: object.properties.color, opacity: object.properties.opacity, transparent: true})
-//            );
-//            plane.position.z = object.properties.position.z;
-//            plane.rotation.x = 180 * (Math.PI / 180);
-//            this.scene.add(plane);
-//
-//            break;
-//
-//        case "mesh":
-//            break;
-//        }
+        _LD("    <GraphicsEngine::loadObject.");
+
     };
 
+    /**
+     * The frame method :o
+     * @returns {undefined}
+     */
     GraphicsEngine.prototype.frame = function () {
-//        if (this.gameScene === null) return;
-
-        //this.globalObjects[0].rotation.x += 0.05;
-        //this.plane.rotation.y += 0.2;
-
-    //    this.plane.position.z += 0.5;
-  //      this.camera.lookAt(this.plane.position);
-
-//        console.log(this.plane.position);
-        /*this.camera.position.x += 0.1;
-        this.camera.position.y += 0.1;
-        this.camera.position.z += 0.1;
         
-        this.camera.lookAt(this.scene.position);*/
-//
-//        for (var i = 0; i < this.gameScene.gameObjects.length; i++) {
-//            if (this.gameScene.gameObjects[i].renderable) {
-//
-//                this.gameScene.gameObjects[i].renderable.position.x = this.gameScene.gameObjects[i].properties.desiredX;
-//            }
-//        }
+        if (this.gameScene === null) {
+            return;
+        }
 
+        this.controlsHelper.frame();
+        
         this.renderer.render(this.scene, this.camera);
     };
 
